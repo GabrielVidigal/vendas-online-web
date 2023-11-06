2ddf4e36ebf641bddb6712135544b92225a9b068
Gabriel Vidigal <gabrielcvfcvf@gmail.com>
2023-11-06 12:34:29 -0300

    feat: refactor requests

--- src/modules/firstScreen/screens/FirstScreen.tsx
+++ src/modules/firstScreen/screens/FirstScreen.tsx
@@ -1,18 +1,18 @@
 import { Spin } from 'antd';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import { useGlobalContext } from '../../../shared/hooks/useGlobalContext';
 import { ProductRoutesEnum } from '../../product/routes';
 
 const FirstScreen = () => {
-  const { user } = useGlobalContext()
+  const { user } = useGlobalContext();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
-      navigate(ProductRoutesEnum.PRODUCT)
+      navigate(ProductRoutesEnum.PRODUCT);
     }
-    
   }, []);
   return <Spin />;
 };
