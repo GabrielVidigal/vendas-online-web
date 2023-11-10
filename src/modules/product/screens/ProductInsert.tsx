2440f2e7c795705b437c58ceb1074aaedf08dcd9
Gabriel Vidigal <gabrielcvfcvf@gmail.com>
2023-11-10 15:15:49 -0300

    feat: refactor insert product

--- src/modules/product/screens/ProductInsert.tsx
+++ src/modules/product/screens/ProductInsert.tsx
@@ -2,25 +2,31 @@ import { useEffect, useState } from 'react';
 
 import Button from '../../../shared/components/buttons/button/Button';
 import Input from '../../../shared/components/inputs/input/input';
+import Select from '../../../shared/components/inputs/select/Select';
 import Screen from '../../../shared/components/screen/Screen';
 import { URL_CATEGORY, URL_PRODUCT } from '../../../shared/constants/urls';
 import { MethodsEnum } from '../../../shared/enums/methods.enum';
+import { connectionAPIPost } from '../../../shared/functions/connection/connectionAPI';
 import { useDataContext } from '../../../shared/hooks/useDataContext';
 import { useRequests } from '../../../shared/hooks/useRequests';
-import { ProductRoutesEnum } from '../routes';
-import { LimitedContainer } from '../styles/productInsert.style';
-import Select from '../../../shared/components/inputs/select/Select';
 import { InsertProduct } from '../../../shared/types/dtos/InsertProduct.dto';
-import { connectionAPIPost } from '../../../shared/functions/connection/connectionAPI';
+import { ProductRoutesEnum } from '../routes';
+import { ProductInsertContainer } from '../styles/productInsert.style';
+import { LimitedContainer } from '../../../shared/styles/limited.styled';
+import { DisplayFlexJustifyRight } from '../../../shared/styles/display.styled';
+import { useNavigate } from 'react-router-dom';
+import { useGlobalContext } from '../../../shared/hooks/useGlobalContext';
 
 const ProductInsert = () => {
   const [product, setProduct] = useState<InsertProduct>({
     name: '',
     price: 0,
     image: '',
-    });
+  });
   const { categories, setCategories } = useDataContext();
+  const { setNotification } = useGlobalContext();
   const { request } = useRequests();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (categories.length === 0) {
@@ -28,30 +34,39 @@ const ProductInsert = () => {
     }
   }, []);
 
-  const handleInsertProduct = () => {
-    connectionAPIPost(URL_PRODUCT, product)
-  }
+  const handleInsertProduct = async () => {
+    await connectionAPIPost(URL_PRODUCT, product)
+      .then(() => {
+        setNotification('Sucesso!', 'success', 'Produto inserido com sucesso!');
+        navigate(ProductRoutesEnum.PRODUCT);
+      })
+      .catch((error: Error) => {
+        setNotification(error.message, 'error');
+      });
+  };
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>, nameObject: string) => {
+  const handleOnClickCancel = () => {
+    navigate(ProductRoutesEnum.PRODUCT);
+  };
+
+  const onChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    nameObject: string,
+    isNumber?: boolean,
+  ) => {
     setProduct({
       ...product,
-      [nameObject]: event.target.value
-    })
-  }
-
-  const onChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setProduct({
-      ...product,
-      price: Number(event.target.value)
-    })
-  }
+      [nameObject]: isNumber ? Number(event.target.value) : event.target.value,
+    });
+  };
 
   const handleChange = (value: string) => {
     setProduct({
       ...product,
-      categoryId: Number(value)
-    })
+      categoryId: Number(value),
+    });
   };
+
   return (
     <Screen
       listBreadcrumb={[
@@ -67,21 +82,52 @@ const ProductInsert = () => {
         },
       ]}
     >
-      <LimitedContainer>
-        <Input onChange={(event) => onChange(event, 'name')} value={product.name} margin="0px 0px 16px 0 px" title="Nome" placeholder="Nome" />
-        <Input onChange={(event) => onChange(event, 'image')} value={product.image} margin="0px 0px 16px 0 px" title="Url imagem" placeholder="Url imagem" />
-        <Input onChange={onChangePrice} value={product.price} margin="0px 0px 16px 0 px" title="Preço" placeholder="Preço" />
-        <Select
-          title='Categoria'
-          margin="0px 0px 32px 0 px"
-          onChange={handleChange}
-          options={categories.map((category) => ({
-            value: `${category.id}`,
-            label: `${category.name}`,
-          }))}
-        />
-        <Button onClick={handleInsertProduct} type="primary"> Inserir produto </Button>
-      </LimitedContainer>
+      <ProductInsertContainer>
+        <LimitedContainer width={400}>
+          <Input
+            onChange={(event) => onChange(event, 'name')}
+            value={product.name}
+            margin="0px 0px 16px 0px"
+            title="Nome"
+            placeholder="Nome"
+          />
+          <Input
+            onChange={(event) => onChange(event, 'image')}
+            value={product.image}
+            margin="0px 0px 16px 0px"
+            title="Url imagem"
+            placeholder="Url imagem"
+          />
+          <Input
+            onChange={(event) => onChange(event, 'price', true)}
+            value={product.price}
+            margin="0px 0px 16px 0px"
+            title="Preço"
+            placeholder="Preço"
+          />
+          <Select
+            title="Categoria"
+            margin="0px 0px 32px 0px"
+            onChange={handleChange}
+            options={categories.map((category) => ({
+              value: `${category.id}`,
+              label: `${category.name}`,
+            }))}
+          />
+          <DisplayFlexJustifyRight>
+            <LimitedContainer margin="0px 8px" width={120}>
+              <Button danger onClick={handleOnClickCancel}>
+                Cancelar
+              </Button>
+            </LimitedContainer>
+            <LimitedContainer width={120}>
+              <Button onClick={handleInsertProduct} type="primary">
+                Inserir produto
+              </Button>
+            </LimitedContainer>
+          </DisplayFlexJustifyRight>
+        </LimitedContainer>
+      </ProductInsertContainer>
     </Screen>
   );
 };
